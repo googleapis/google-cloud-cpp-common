14b615d992b9319734a34810603d278f7d6c85c9
Carlos O'Ryan <coryan@users.noreply.github.com>
2019-10-03 22:14:10 -0400

    feat: configure build on macOS with Bazel (#16)

--- ci/kokoro/macos/build.sh
+++ ci/kokoro/macos/build.sh
@@ -16,9 +16,6 @@
 
 set -eu
 
-echo "Reading CI secret configuration parameters."
-source "${KOKORO_GFILE_DIR}/test-configuration.sh"
-
 echo "Running build and tests"
 cd "$(dirname "$0")/../../.."
 readonly PROJECT_ROOT="${PWD}"
@@ -38,20 +35,10 @@ function sha256sum() { shasum -a 256 "$@" ; } && export -f sha256sum
 readonly BAZEL_BIN="$HOME/bin/bazel"
 echo "Using Bazel in ${BAZEL_BIN}"
 
-# We need this environment variable because on macOS gRPC crashes if it cannot
-# find the credentials, even if you do not use them. Some of the unit tests do
-# exactly that.
-echo
-echo "================================================================"
-echo "================================================================"
-echo "Define GOOGLE_APPLICATION_CREDENTIALS."
-export GOOGLE_APPLICATION_CREDENTIALS="${KOKORO_GFILE_DIR}/service-account.json"
-
 # The -DGRPC_BAZEL_BUILD is needed because gRPC does not compile on macOS unless
 # it is set.
 "${BAZEL_BIN}" test \
     --copt=-DGRPC_BAZEL_BUILD \
-    --action_env=GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS}" \
     --test_output=errors \
     --verbose_failures=true \
     --keep_going \
@@ -62,7 +49,6 @@ echo "================================================================"
 echo "================================================================"
 "${BAZEL_BIN}" build \
     --copt=-DGRPC_BAZEL_BUILD \
-    --action_env=GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS}" \
     --test_output=errors \
     --verbose_failures=true \
     --keep_going \
