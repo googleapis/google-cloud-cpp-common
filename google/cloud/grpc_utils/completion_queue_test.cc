762d3d75284534c785ba9fd75c9022010e8dadc1
Carlos O'Ryan <coryan@google.com>
2020-01-29 14:24:10 -0500

    Also test RunAsync()

--- google/cloud/grpc_utils/completion_queue_test.cc
+++ google/cloud/grpc_utils/completion_queue_test.cc
@@ -267,6 +267,25 @@ TEST(CompletionQueueTest, MakeStreamingReadRpc) {
   runner.join();
 }
 
+TEST(CompletionQueueTest, RunAsync) {
+  using ms = std::chrono::milliseconds;
+  CompletionQueue cq;
+
+  std::thread runner([&cq] { cq.Run(); });
+
+  std::promise<void> done_promise;
+  cq.RunAsync([&done_promise](CompletionQueue&) {
+    done_promise.set_value();
+  });
+
+  auto done = done_promise.get_future();
+  EXPECT_EQ(std::future_status::ready, done.wait_for(ms(10)));
+  done.get();
+
+  cq.Shutdown();
+  runner.join();
+}
+
 }  // namespace
 }  // namespace GOOGLE_CLOUD_CPP_GRPC_UTILS_NS
 }  // namespace grpc_utils
